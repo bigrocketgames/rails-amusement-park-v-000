3aa07a3d7db90f2f020a2d843059a69da5b5fcc4
NATHAN ROBBERT <admin@bigrocketgames.com>
2017-02-01 00:23:59 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def logged_in?
+    !!current_user
+  end
+
+  private
+
+  def current_user
+    @user ||= User.find(session[:user_id] if session[:user_id])
+  end
+  helper_method :current_user
+
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,20 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    if (user.tickets < attraction.tickets) && (user.height < attraction.min_height)
+      "Sorry. You do not have enough tickets the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif (user.tickets < attraction.tickets)
+      "Sorry. You do not have enough tickets the #{attraction.name}."
+    elsif (user.height < attraction.min_height)
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
+    else
+      new_tickets = self.user.tickets - self.attraction.tickets
+      new_nausea = self.user.nausea + self.attraction.nausea_rating
+      new_happiness = self.user.happiness + self.attraction.happiness_rating
+      self.user.update(:happiness => new_happiness, :nausea => new_nausea, :tickets => new_tickets)
+    end
+  end
+
 end
