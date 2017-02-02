9c2fee863875180a868dc0a6a34fa9b7724a5b2c
Nathan Robbert <admin@bigrocketgames.com>
2017-02-01 21:49:43 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    @user ||= User.find(session[:user_id] if session[:user_id])
+    @user ||= User.find((session[:user_id] if session[:user_id]))
   end
   helper_method :current_user
 
